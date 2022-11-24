bab92be77930304773b641a255605ff8b7833613
Nick Buk[0vec] <nbukovec@calpoly.edu>
2022-11-23 16:12:02 -0800

    :lipstick: configured tailwind + skeleton ui
    Installed and configured tailwind and Skeleton UI with a simple header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../theme.postcss';
+	import '@brainandbones/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { AppShell, AppBar } from '@brainandbones/skeleton';
+</script>
+
+<AppShell>
+	<!-- Header -->
+	<svelte:fragment slot="header"><h1>Rice</h1></svelte:fragment>
+	<!-- Page Content Slot -->
+	<slot />
+</AppShell>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		require('path').join(require.resolve('@brainandbones/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	darkMode: 'class',
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('@brainandbones/skeleton/tailwind/theme.cjs')]
+};
+
+module.exports = config;

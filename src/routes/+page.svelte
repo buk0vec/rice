ad4ac8445606569f8249df188cc47d07e8d1ae93
Nick Buk[0vec] <nbukovec@calpoly.edu>
2022-11-23 16:40:40 -0800

    :sparkles: landing page
    Created a basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center">
+	<h1 class="font-semibold text-7xl text-center p-4">Rice</h1>
+	<div class="flex flex-row gap-4">
+		<a class="btn btn-filled-primary w-min" href="/app">Sign in</a>
+		<a class="btn btn-filled-primary w-min" href="/app">Create account</a>
+	</div>
+</div>

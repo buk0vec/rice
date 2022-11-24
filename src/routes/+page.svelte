e3d900c336e726914189cdcae68a8cd44de52f9b
Nick Buk[0vec] <nbukovec@calpoly.edu>
2022-11-23 16:43:13 -0800

    :sparkles: app page
    Added basic app page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,6 @@
 	<h1 class="font-semibold text-7xl text-center p-4">Rice</h1>
 	<div class="flex flex-row gap-4">
 		<a class="btn btn-filled-primary w-min" href="/app">Sign in</a>
-		<a class="btn btn-filled-primary w-min" href="/app">Create account</a>
+		<a class="btn btn-filled-primary w-min" href="/signup">Create account</a>
 	</div>
 </div>
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center">
+	<h1 class="font-semibold text-7xl text-center p-4">Main Application</h1>
+	<a class="btn btn-filled-primary w-min" href="/">Sign out</a>
+</div>

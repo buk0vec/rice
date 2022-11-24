65ec1d033d59d9da0a2f1ca70f8b836901329ccf
Nick Buk[0vec] <nbukovec@calpoly.edu>
2022-11-23 16:28:48 -0800

    :lipstick: Added header bar
    Added header bar to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,21 @@
 
 <AppShell>
 	<!-- Header -->
-	<svelte:fragment slot="header"><h1>Rice</h1></svelte:fragment>
+	<svelte:fragment slot="header"
+		><AppBar>
+			<svelte:fragment slot="lead">
+				<img width="64px" height="64px" src="android-chrome-192x192.png" alt="Rice logo" />
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm"
+					href="https://github.com/buk0vec/rice"
+					target="_blank"
+					rel="noreferrer">GitHub</a
+				>
+			</svelte:fragment>
+		</AppBar></svelte:fragment
+	>
 	<!-- Page Content Slot -->
 	<slot />
 </AppShell>
